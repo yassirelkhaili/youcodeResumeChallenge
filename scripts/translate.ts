3c53b03d6eac33c5685a9e6dd488610eff7bd1f8
Yassir Elkhaili <elkhailiyassir@gmail.com>
2024-10-05 08:46:45 +0200

    Update translate.ts

--- scripts/translate.ts
+++ scripts/translate.ts
@@ -108,10 +108,10 @@ const translations: Record<Language, Translations> = {
     project2_skills: "Required Skills: PHP, SQL, design/architectural patterns, ORM design",
     project3_skills: "Required Skills: TailwindCSS, Typescript, ReactJS, PHP, SimpleKit, UML, SQL",
     project4_skills: "Required Skills: TailwindCSS, Javascript, Typescript, NodeJS",
-    position4_title: "Software Developer Intern",
-    position4_phrase1: "Completed required tasks and contributed to enhancing the company's internal framework.",
-    position4_phrase2: "Collaborated with coworkers to achieve daily objectives and meet project goals.",
-    position4_phrase3: "Enhanced the overall user experience and efficiency of ePhilos AG's main SaaS solution.",
+    position4_title: "Developer Intern",
+    position4_phrase1: "Completed tasks and enhanced the company's internal framework.",
+    position4_phrase2: "collaborated with coworkers to achieve objectives and meet goals.",
+    position4_phrase3: "Enhanced the user experience and efficiency of the main solution.",
     position4_phrase4: "Required Skills: Advanced JavaScript, PHP, SQL, Internal framework."
   },
   FR: {
